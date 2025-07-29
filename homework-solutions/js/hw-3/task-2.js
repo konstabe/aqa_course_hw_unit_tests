5dc167ffd1e8e0320cf232a61d2dfb71378f4867
konstabe <konstantingames94@gmail.com>
2025-07-29 20:50:29 +0300

    HW-3

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -1,6 +1,15 @@
-/*
+const concat = (n) => {
+    if (typeof n !== "number") {
+        return "должно быть число";
+    }
 
-Напишите программу, которая принимает целое положительное число n (одно любое число от 1 до 9), и выводит сумму равную 
-n + nn + nnn, где n не перемножаются, а конкатенируются
+    if (n < 1 || n > 9) {
+        return "должно быть число от 1 до 9";
+    }
 
-*/
+    const n1 = String(n);
+    const n2 = String(n) + String(n);
+    const n3 = String(n) + String(n) + String(n);
+
+    return Number(n1) + Number(n2) + Number(n3);
+}
