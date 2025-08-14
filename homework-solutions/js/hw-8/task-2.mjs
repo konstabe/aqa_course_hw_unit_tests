3605fc3e9ed3d90a1b068a3e86b63f759ae50e5a
konstabe <konstantingames94@gmail.com>
2025-08-14 17:42:10 +0300

    fixes

--- homework-solutions/js/hw-8/task-2.mjs
+++ homework-solutions/js/hw-8/task-2.mjs
@@ -18,30 +18,13 @@ const words = [
   'queue',
 ];
 
-function sortedByVowels(wordsArr) {
+function vowelsCounter (phrase) {
   const vowels = new Set(['e', 'y', 'u', 'i', 'o', 'a']);
-  const vowelsCounter = (phrase) => {
-    return [...phrase].reduce((result, word) => result + vowels.has(word), 0);
-  };
+  return [...phrase].reduce((result, word) => result + vowels.has(word.toLowerCase()), 0);
+};
 
+function sortedByVowels (wordsArr) {
   return wordsArr.sort((a, b) => vowelsCounter(a) - vowelsCounter(b));
-  
-  // return wordsArr.sort((a, b)=>{
-  //   const aWordArray = a.split('');
-  //   const bWordArray = b.split('');
-
-  //   const aVowels = vowels.reduce((result, vowel) =>{
-  //     result += aWordArray.filter(word => word === vowel).length;
-  //     return result;
-  //   }, 0);
-
-  //   const bVowels = vowels.reduce((result, vowel) =>{
-  //     result += bWordArray.filter(word => word === vowel).length;
-  //     return result;
-  //   }, 0);
-
-  //   return aVowels - bVowels;
-  // });
 }
 
 export { sortedByVowels };
--- homework-solutions/js/hw-8/task-3.mjs
+++ homework-solutions/js/hw-8/task-3.mjs
@@ -9,7 +9,7 @@
 function findMissingNumber(numbers) {
   sortedNumbers = [...numbers].sort ((a, b) => a - b);
 
-  const expectedArray = Array.from({length: 99}, (_, i) => i + 1);
+  const expectedArray = Array.from({length: numbers.length + 1}, (_, i) => i + 1);
   return expectedArray.find((number, index) => number !== sortedNumbers[index]);
 }
 
